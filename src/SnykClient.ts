2af90cb03fd80b3c4c8c0dd5ca727e188098de4a
Joel Collyer <jcollyer@lodgelink.com>
2024-02-20 16:58:46 -0700

    Adding API version to SnykClient

--- src/SnykClient.ts
+++ src/SnykClient.ts
@@ -1,6 +1,8 @@
 interface ISnykClient {
   headers: HeadersInit;
   project: string;
+  version: string;
+  url: string;
   getCollections(): Promise<SnykCollection[]>;
 }
 
@@ -12,21 +14,28 @@ export interface SnykCollection {
 class SnykClient implements ISnykClient {
   headers;
   project;
+  version;
   url;
 
   constructor() {
     const { VITE_SNYK_ACCESS_TOKEN, VITE_SNYK_PROJECT, VITE_SNYK_URL } =
       import.meta.env;
+
+    // Validation token information is from: https://apidocs.snyk.io/?version=2024-01-23#overview
     this.headers = {
-      Authorization: `Token ${VITE_SNYK_ACCESS_TOKEN}`,
+      Authorization: `token ${VITE_SNYK_ACCESS_TOKEN}`,
       "Content-Type": "application/vnd.api+json",
     };
     this.project = VITE_SNYK_PROJECT;
     this.url = VITE_SNYK_URL;
+    this.version = "?version=2024-01-23";
   }
 
+  /**
+   * https://apidocs.snyk.io/?version=2024-01-23#get-/orgs/-org_id-/collections
+   */
   async getCollections() {
-    const path = `${this.url}/orgs/${this.project}/collections`;
+    const path = `${this.url}/orgs/${this.project}/collections${this.version}`;
     const collections = await fetch(path, {
       headers: this.headers,
       mode: "no-cors",
